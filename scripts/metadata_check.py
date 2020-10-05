d8a0579cf9480ba86a17c265bc78b36fa83844ef
Adriane Hanson <ahansonuga@gmail.com>
2020-10-05 10:56:46 -0400

    Change how seed report is made
    Instead of setting up csvs for all known departments (collectors), makes the csvs or adds to them as it iterates through the seeds for more flexibility.

--- scripts/metadata_check.py
+++ scripts/metadata_check.py
@@ -15,6 +15,7 @@ import configuration as c
 def get_metadata_value(data, field):
     """Looks up the value of a field in the Archive-It API output for a particular collection or seed. If the field
     is not in the output, returns the string NONE instead."""
+    # TODO: if there is more than one value for the same metadata field, this only gets the value of one. See seed language for example.
     try:
         return data['metadata'][field][0]['value']
     except KeyError:
@@ -66,51 +67,44 @@ for coll_data in py_collections:
                             collection_metadata_page])
 
 
-# # PART TWO: SEED REPORTS
-#
-# # Gets the Archive-It seed report with data on all the seeds.
-# seeds = requests.get(f'{c.partner_api}/seed?limit=-1', auth=(c.username, c.password))
-#
-# # If there was an error with the API call, quits the script.
-# if not seeds.status_code == 200:
-#     print('Error with Archive-It API connection when getting seed report', seeds.status_code)
-#     exit()
-#
-# # Saves the seed data as a Python object.
-# py_seeds = seeds.json()
-#
-# # Makes CSVs (one per department) to save results to, including adding header rows.
-# with open('hargrett_seeds_metadata.csv', 'w', newline='') as harg_output, open('russell_seeds_metadata.csv', 'w', newline='') as rbrl_output, open('none_seeds_metadata.csv', 'w', newline='') as none_output:
-#     seed_header = ['Seed ID', 'URL', 'Collector', 'Creator', 'Date', 'Identifier', 'Language', 'Rights', 'Title', 'Metadata Page']
-#     harg_write = csv.writer(harg_output)
-#     harg_write.writerow(seed_header)
-#     rbrl_write = csv.writer(rbrl_output)
-#     rbrl_write.writerow([seed_header])
-#     none_write = csv.writer(none_output)
-#     none_write.writerow([seed_header])
-#
-#     # Iterates over the metadata for each seed.
-#     for seed_data in py_seeds:
-#
-#         # Constructs the URL of the seed's metadata page to make it easy to edit a record.
-#         seed_metadata_page = f"{c.inst_page}/collections/{seed_data['collection']}/seeds/{seed_data['id']}/metadata"
-#
-#         # Gets the values of the required metadata fields (or 'NONE' if the field has no metadata).
-#         collector = get_metadata_value(seed_data, 'Collector')
-#         creator = get_metadata_value(seed_data, 'Creator')
-#         date = get_metadata_value(seed_data, 'Date')
-#         identifier = get_metadata_value(seed_data, 'Identifier')
-#         language = get_metadata_value(seed_data, 'Language')
-#         rights = get_metadata_value(seed_data, 'Rights')
-#         title = get_metadata_value(seed_data, 'Title')
-#
-#         # Adds a row to the appropriate CSV with the metadata for the seed.
-#         if collector == 'Hargrett Rare Book & Manuscript Library':
-#             harg_write.writerow([seed_data['id'], seed_data['url'], collector, creator, date, identifier, language,
-#                                  rights, title, seed_metadata_page])
-#         elif collector == 'NONE':
-#             none_write.writerow([seed_data['id'], seed_data['url'], collector, creator, date, identifier, language,
-#                                  rights, title, seed_metadata_page])
-#         else:
-#             rbrl_write.writerow([seed_data['id'], seed_data['url'], collector, creator, date, identifier, language,
-#                                  rights, title, seed_metadata_page])
+# PART TWO: SEED REPORTS
+
+# Gets the Archive-It seed report with data on all the seeds.
+seeds = requests.get(f'{c.partner_api}/seed?limit=-1', auth=(c.username, c.password))
+
+# If there was an error with the API call, quits the script.
+if not seeds.status_code == 200:
+    print('Error with Archive-It API connection when getting seed report', seeds.status_code)
+    exit()
+
+# Saves the seed data as a Python object.
+py_seeds = seeds.json()
+
+# Iterates over the metadata for each seed.
+for seed_data in py_seeds:
+
+    # Constructs the URL of the seed's metadata page to make it easy to edit a record.
+    seed_metadata_page = f"{c.inst_page}/collections/{seed_data['collection']}/seeds/{seed_data['id']}/metadata"
+
+    # Gets the values of the required metadata fields (or 'NONE' if the field has no metadata).
+    collector = get_metadata_value(seed_data, 'Collector')
+    creator = get_metadata_value(seed_data, 'Creator')
+    date = get_metadata_value(seed_data, 'Date')
+    identifier = get_metadata_value(seed_data, 'Identifier')
+    language = get_metadata_value(seed_data, 'Language')
+    rights = get_metadata_value(seed_data, 'Rights')
+    title = get_metadata_value(seed_data, 'Title')
+
+    # If this is the first seed for this department, makes a CSV with a header row for seed metadata.
+    if not os.path.exists(f'{collector}_seeds_metadata.csv'):
+        with open(f'{collector}_seeds_metadata.csv', 'w', newline='') as output:
+            seed_header = ['Seed ID', 'URL', 'Collector', 'Creator', 'Date', 'Identifier', 'Language', 'Rights',
+                           'Title', 'Metadata Page']
+            header_write = csv.writer(output)
+            header_write.writerow(seed_header)
+
+    # Adds the seed metadata as a row to the department's CSV.
+    with open(f'{collector}_seeds_metadata.csv', 'a', newline='') as output:
+        row_write = csv.writer(output)
+        row_write.writerow([seed_data['id'], seed_data['url'], collector, creator, date, identifier, language, rights,
+                            title, seed_metadata_page])
