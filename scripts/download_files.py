de480b7bd968c4a1a9f9367aa1a9ad1410f02226
Adriane Hanson <ahansonuga@gmail.com>
2021-03-10 13:24:20 -0500

    Make a list of URLs from the input csvs, download_files.py

--- scripts/download_files.py
+++ scripts/download_files.py
@@ -5,8 +5,9 @@ Designed for PDF-only crawls, where publications need to be saved and cataloged 
 Prior to running the script, download the File Type List for every file type and every crawl to include and
 save the CSVs to a single folder. These files are the script input.
 
-Future development idea: want to be able to review the URLS and remove unwanted things prior to download?
-Could combine the URLs to a single CSV for staff review and do the download from that CSV.
+Future development ideas:
+    * Save all URLs to a CSV for staff review to remove unwanted things prior to download?
+    * In test, some showed as duplicate in the CSV, although is only in the CSV once. Skip because is in another crawl?
 """
 
 # Usage: python /path/download_files.py /path/input_directory
@@ -26,6 +27,24 @@ except (IndexError, FileNotFoundError, NotADirectoryError):
     exit()
 
 # Gets the document URLs from each CSV in the input folder and saves them to a list.
+download_urls = []
+for input_csv in os.listdir("."):
+
+    # Reads each row in the CSV file.
+    with open(input_csv) as csvfile:
+        data = csv.reader(csvfile)
+
+        # Verify the first column name in the first row is "url".
+        # If not, this CSV is not formatted as expected. Prints an error and starts the next CSV.
+        header = next(data)
+        print(header)
+        if not header[0] == "url":
+            print("This file is not formatted correctly and will be skipped:", input_csv)
+            break
+
+        # Gets the URL, which is the first item in the row, and saves to the URLS list.
+        for row in data:
+            download_urls.append(row[0])
 
 # Downloads the document for each URL in the list and saves it to a folder named with the seed URL.
 
