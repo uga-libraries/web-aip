df02eac526c9052bf4d3904da2bfd914d306ac32
Adriane Hanson <ahansonuga@gmail.com>
2021-03-11 15:58:36 -0500

    update notes, download_files.py
    Script is a working proof of concept. Need to talk to staff about configuration options and test for url variations more deeply.

--- scripts/download_files.py
+++ scripts/download_files.py
@@ -1,20 +1,22 @@
 """
-Download individual files from crawled websites using the URLs from the File Types lists.
+Download individual PDFs from crawled websites saved in Archive-It using the URLs from the File Types lists.
 Designed for PDF-only crawls, where publications need to be saved and cataloged separately.
 
-Prior to running the script, download the File Type List for every file type and every crawl to include and
+Prior to running the script, download the File Type List for PDFs from Archive-It for every crawl to include and
 save the CSVs to a single folder. These files are the script input.
 
 Dependency: wget https://www.gnu.org/software/wget/
 
 Future development ideas:
     * Save all URLs to a CSV for staff review to remove unwanted things prior to download?
-    * Expand beyond PDF? Need to change how renamed.
-    * Need to be able to download from more than one collection at a time?
-    * Want any logging, summary statistics about how many were downloaded, and/or showing progress?
+    * Adapt script for other formats besides PDF? Just need to change how files are renamed.
+    * Need to be able to download from more than one collection at once?
+    * Want any logging, summary statistics about how many were downloaded, and/or showing script progress?
 """
 
 # Usage: python /path/download_files.py /path/input_directory archiveit_collection_id
+
+# WARNING: THIS SCRIPT IS A PROOF OF CONCEPT AND HAS BEEN MINIMALLY TESTED
 
 import csv
 import os
@@ -34,7 +36,6 @@ except (IndexError, FileNotFoundError, NotADirectoryError):
 
 # Gets the Archive-It collection id from the script argument, which is used for making the Archive-It URL.
 # If the id is missing, prints an error and quits the script.
-# TODO: could also check if the id is a number.
 try:
     collection = sys.argv[2]
 except IndexError:
@@ -42,43 +43,37 @@ except IndexError:
     print("Script usage: python /path/download_files.py /path/input_directory archiveit_collection_id")
     exit()
 
-# Gets the document URLs from each CSV in the input folder and saves them to a dictionary.
-# The dictionary keys are the seeds and values are lists of URLS for that seeds.
+# Gets the PDF URLs from each CSV in the input folder and saves them to a dictionary.
+# The dictionary keys are the seeds and values are a list of URLS for each seed.
 download_urls = {}
 for input_csv in os.listdir("."):
-
-    # Reads each row in the CSV file.
     with open(input_csv) as csvfile:
         data = csv.reader(csvfile)
 
-        # Verify the headers have the expected values. If not, prints an error and starts the next CSV.
+        # Verifies the header has the expected values. If not, prints an error and starts the next CSV.
         header = next(data)
         if not header == ["url", "size", "is_duplicate", "seed"]:
             print("This file is not formatted correctly and will be skipped:", input_csv)
             continue
 
-        # Add each URL to the download_urls dictionary if it is not a duplicate in Archive-It or the dictionary.
+        # Adds the seed to the download_urls dictionary if it is not already present.
+        # Adds each URL to the download_urls dictionary if it is not a duplicate in Archive-It (value of 1 in the CSV).
         for row in data:
-
-            # If the URL is a duplicate from Archive-It, skip it and start the next URL.
-            if row[2] == "1":
+            url, size, is_duplicate, seed = row
+            if is_duplicate == "1":
                 continue
-
-            # Add the seed to download_urls dictionary if not already there, with this URL as its first value.
-            # Otherwise, adds the URL to the download_urls dictionary list for its seed.
-            # TODO: is it possible for one url to be repeated but be for different documents?
-            if not row[3] in download_urls:
-                download_urls[row[3]] = [row[0]]
+            if seed in download_urls:
+                download_urls[seed].append(url)
             else:
-                download_urls[row[3]].append(row[0])
+                download_urls[seed] = [url]
 
-
-# For each seed, downloads the document for each URL in the list and saves it to a folder named with the seed.
+# For each seed, downloads the PDF for each URL in the list and saves it to a folder named with the seed.
 for seed in download_urls.keys():
 
     # Makes a version of the seed URL which can be used for a folder name.
     # Removes http:// or https:// from the beginning if present, / from the end if present, and replaces other / with _
     # TODO: try again with regular expressions. I wasn't getting the optional / at the end to work right the first try.
+    # TODO: are there other characters which could be a problem?
     seed_directory_name = seed
     if seed_directory_name.startswith("http://"):
         seed_directory_name = seed.replace("http://", "")
@@ -88,35 +83,39 @@ for seed in download_urls.keys():
         seed_directory_name = seed_directory_name[:-1]
     seed_directory_name = seed_directory_name.replace("/", "_")
 
-    # Makes a folder for the seed and makes it the current directory.
+    # Makes a folder for the seed and makes it the current directory so wget can save the PDFs there.
+    # TODO: add error handling for if the name has illegal characters.
     os.makedirs(os.path.join(input_directory, seed_directory_name))
     os.chdir(os.path.join(input_directory, seed_directory_name))
 
-    # Saves the document, with the desired file name, to the seed folder.
+    # Saves the PDF, with the desired file name, to the seed folder.
     for url in download_urls[seed]:
 
-        # Calculates the desired name for the file. Generally, this is the last part of the URL plus .pdf.
-        # If the last part of the URL is download, gets the previous part of the URL.
-        # If the last part of the URL is pdf, removes that before adding .pdf extension.
-        # TODO there may be other generic naming conventions to address as well
+        # Makes the desired name for the file. Generally, this is the last part of the URL plus ".pdf".
+        # If the last part of the URL is download, gets the previous part of the URL instead.
+        # TODO there may be other generic naming conventions to address as well.
         if url.endswith("download"):
             regex = re.match("(.*)/download", url)
             filename = regex.group(1) + ".pdf"
         else:
             regex = re.match(".*/(.*)", url)
+            # If the last part of the URL already has a pdf extension, does not add ".pdf" to it.
             if url.endswith(".pdf") or url.endswith(".PDF"):
                 filename = regex.group(1)
+            # If the last part of the URL is pdf without the period, removes the "pdf" before adding the .pdf extension.
             elif url.endswith("pdf") or url.endswith("PDF"):
                 filename = regex.group(1)[:-3] + ".pdf"
             else:
                 filename = regex.group(1) + ".pdf"
 
-        # Calculates the URL in Archive-It by adding the Wayback URL, the collection, and 3 for the most recent capture.
+        # Makes the URL in Archive-It by adding the Wayback URL, the collection id, and 3 for the most recent capture.
         # TODO: this may only work on Windows because of the direction of the slashes.
         # Can't use os.path.join because url already has slashes in it.
         archiveit_url = f"https://wayback.archive-it.org/{collection}/3/{url}"
 
-        # Downloads the PDF to the seed's directory, named with the desired name.
-        # TODO: prints a lot to the terminal. Make it quiet?
+        # Saves the PDF to the seed's directory, named with the desired name.
+        # TODO: Can end up with more than one different file that has the same name, and only one is saved.
+        #   wget non-clobber will not download if already present, rather than download with a rename.
+        # TODO: prints a lot to the terminal. Make it quiet? Or like to see the script progress?
         # TODO: error handling
         subprocess.run(f'wget -O "{filename}" "{archiveit_url}"', shell=True)
