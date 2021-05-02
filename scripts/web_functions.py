19d0d95b9471dc49f257ad2017ca97ecf4aac17e
Adriane Hanson <ahansonuga@gmail.com>
2021-05-01 23:32:29 -0400

    Fix logic for logging status error for metadata report, download_metadata()
    Caused log of 200 as an error (which it isn't) if the path does exist.

--- scripts/web_functions.py
+++ scripts/web_functions.py
@@ -236,9 +236,10 @@ def download_metadata(aip_id, aip_folder, warc_collection, job_id, seed_id, curr
         # Saves the metadata report if there were no errors with the API and the report has not already been downloaded.
         # If there is more than one WARC for a seed, reports may already be in the metadata folder.
         report_path = f'{c.script_output}/aips_{current_download}/{aip_folder}/metadata/{report_name}'
-        if metadata_report.status_code == 200 and not os.path.exists(report_path):
-            with open(f'{aip_folder}/metadata/{report_name}', 'wb') as report_csv:
-                report_csv.write(metadata_report.content)
+        if metadata_report.status_code == 200:
+            if not os.path.exists(report_path):
+                with open(f'{aip_folder}/metadata/{report_name}', 'wb') as report_csv:
+                    report_csv.write(metadata_report.content)
         else:
             aip.log(log_path, f'Could not download {report_type} report. Error: {metadata_report.status_code}')
 
