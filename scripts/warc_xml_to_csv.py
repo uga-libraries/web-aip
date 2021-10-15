ca09e8d9f608357b32e02476594a6a750a2b8583
Adriane Hanson <ahansonuga@gmail.com>
2021-10-15 14:11:06 -0400

    Script cleanup, warc_xml_to_csv.py
    Clarifying notes, including expanding explanation for how to make the script input.
    
    Implementing suggestions from pylint: line length, variable naming conventions, use of sys.exit()

--- scripts/warc_xml_to_csv.py
+++ scripts/warc_xml_to_csv.py
@@ -1,11 +1,15 @@
-# Convert WARC information downloaded from WASAPI as XML into a CSV for analyzing WARC information. The resulting CSV
-# is saved in the same folder as the input.
+"""Convert WARC information downloaded from WASAPI as XML into a CSV for analyzing WARC information.
+The resulting CSV is saved in the same folder as the input XML.
 
-# XML for the script input is downloaded from https://warcs.archive-it.org/wasapi/v1/webdata. Combine the information
-# from all result pages into a single document, with one root and files element and every list-item element. Do not
-# include the elements before or after files.
+To make the script input (warc.xml):
+    1. Log into your Archive-It account.
+    2. Downloaded XML from https://warcs.archive-it.org/wasapi/v1/webdata (under "GET").
+    3. Copy the <root> section to a plain text document and click back on your web browser.
+    4. If there is a URL under "next", go to the next page.
+    5. Get the XML for that page and copy all <list-item> elements to the end of the <files> section.
+    6. Repeat 4-5 until next is null. Save the plain text document as an XML file."""
 
-# Script usage: python /path/warc_xml_to_csv.py /path/warc.xml
+# Script usage: python path\\warc_xml_to_csv.py path\\warc.xml
 
 import csv
 import os
@@ -16,36 +20,36 @@ import xml.etree.ElementTree as et
 
 # Gets the path to the XML file to be converted from the script argument.
 try:
-    warc_xml = sys.argv[1]
+    WARC_XML = sys.argv[1]
 except IndexError:
     print("Including the path to the XML file as a script argument.")
-    print("Script usage: python /path/warc_xml_to_csv.py /path/warc.xml")
-    exit()
+    print("Script usage: python path\\warc_xml_to_csv.py path\\warc.xml")
+    sys.exit()
 
 # Reads the XML file.
 try:
-    tree = et.parse(warc_xml)
+    TREE = et.parse(WARC_XML)
 except FileNotFoundError:
-    print("The path to the XML file is not correct:", warc_xml)
-    print("Script usage: python /path/warc_xml_to_csv.py /path/warc.xml")
-    exit()
+    print("The path to the XML file is not correct:", WARC_XML)
+    print("Script usage: python path\\warc_xml_to_csv.py path\\warc.xml")
+    sys.exit()
 
 # Starts a CSV file, with a header, for the WARC data in the same folder as the XML file.
-warc_xml_folder = os.path.dirname(os.path.abspath(warc_xml))
-warc_csv = open(os.path.join(warc_xml_folder, "converted_warc_xml.csv"), "w", newline="")
-csv_writer = csv.writer(warc_csv)
-csv_writer.writerow(["filename", "collection", "seed", "job", "store-time", "size (GB)"])
+WARC_XML_FOLDER = os.path.dirname(os.path.abspath(WARC_XML))
+WARC_CSV = open(os.path.join(WARC_XML_FOLDER, "converted_warc_xml.csv"), "w", newline="")
+CSV_WRITER = csv.writer(WARC_CSV)
+CSV_WRITER.writerow(["filename", "collection", "seed", "job", "store-time", "size (GB)"])
 
 # Gets the data for each WARC from the XML file.
-root = tree.getroot()
-files = root.find("files")
-for warc in files.findall("list-item"):
+ROOT = TREE.getroot()
+FILES = ROOT.find("files")
+for warc in FILES.findall("list-item"):
     filename = warc.find("filename").text
     collection = warc.find("collection").text
     size = warc.find("size").text
     store_time = warc.find("store-time").text
 
-    # Adds a space before the date so it keeps the original formatting when the CSV is opened in Excel.
+    # Adds a space before the date to keep the original formatting if the file is opened in Excel.
     store_time = " " + store_time
 
     # Gets the job id and seed id from the WARC filename.
@@ -61,6 +65,6 @@ for warc in files.findall("list-item"):
     size = round(float(size) / 1000000000, 2)
 
     # Saves the WARC data as a row in the CSV.
-    csv_writer.writerow([filename, collection, seed_id, job_id, store_time, size])
+    CSV_WRITER.writerow([filename, collection, seed_id, job_id, store_time, size])
 
-warc_csv.close()
+WARC_CSV.close()
