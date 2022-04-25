b7783958b5feacaf7139637b0899cd939dc4d3d0
Adriane Hanson <ahansonuga@gmail.com>
2022-04-25 17:02:46 -0400

    Start for creating a CSV log
    So far, make the log with a header row and start values (in a dictionary) at the start of the warc loop. The rest is commented out because I haven't updated the code yet.

--- scripts/web_aip_batch.py
+++ scripts/web_aip_batch.py
@@ -61,17 +61,12 @@ if not os.path.exists(f'{c.script_output}/{aips_directory}'):
 # Changes current directory to the AIPs folder.
 os.chdir(f'{c.script_output}/{aips_directory}')
 
-# Starts a log for saving status information about the script.
-log_path = f'../web_preservation_download_log_{date_end}.txt'
-aip.log(log_path, f'Creating AIPs for a batch of seeds using the web_aip_batch.py script.\n'
-                  f'Script started running at {datetime.datetime.today()}.')
-
 # PART ONE: DOWNLOAD WARCS AND METADATA INTO AIP DIRECTORY STRUCTURE.
 
 # Uses Archive-It APIs to get information about the WARCs and seeds in this download. If there is an API failure,
 # warc_data() and seed_data() quit the script.
-warc_metadata = web.warc_data(date_start, date_end, log_path)
-seed_metadata = web.seed_data(warc_metadata, date_end, log_path)
+warc_metadata = web.warc_data(date_start, date_end)
+seed_metadata = web.seed_data(warc_metadata, date_end)
 
 # Starts counts for tracking script progress. Some processes are slow, so this shows the script is still working.
 current_warc = 0
@@ -84,144 +79,149 @@ seed_to_aip = {}
 # folder name like the general aip script to allow for using characters in the title which cannot be part of a folder.
 aip_to_title = {}
 
-# Adds name for the next section to the log.
-aip.log(log_path, f'\n\nPROCESSING WARCS ({total_warcs} TOTAL)')
+# Starts a log in the script output folder to use for recording errors related to the warcs.
+web.warc_log("header")
 
 # Iterates through information about each WARC.
 for warc in warc_metadata['files']:
 
+    # Starts a dictionary of information for the log.
+    log_data = {"filename": warc['filename'], "seed_id": "n/a", "warc_json": "n/a", "job_id": "n/a",
+                "seed_metadata": "n/a", "seed_report": "n/a", "seedscope_report": "n/a", "collscope_report": "n/a",
+                "coll_report": "n/a", "crawljob_report": "n/a", "crawldef_report": "n/a", "warc_api": "n/a",
+                "md5deep": "n/a", "fixity": "n/a", "complete": "Errors during WARC processing."}
+
     # Updates the current WARC number and displays the script progress.
     current_warc += 1
-    aip.log(log_path, f"\n{warc['filename']}")
     print(f"\nProcessing {warc['filename']} ({current_warc} of {total_warcs}).")
 
-    # Calculates seed id, which is a portion of the WARC filename between "-SEED" and "-".
-    # Stops processing this WARC and starts the next if the WARC filename doesn't match expected pattern.
-    try:
-        regex_seed_id = re.match(r'^.*-SEED(\d+)-', warc['filename'])
-        seed_id = regex_seed_id.group(1)
-    except AttributeError:
-        aip.log(log_path, f"Cannot calculate seed id from the WARC filename: {warc['filename']}.\n"
-                          f"This WARC and its metadata reports will not be downloaded.")
-        continue
-
-    # Saves relevant information about the WARC in variables for future use.
-    # Stops processing if the WARC does not have the required metadata.
-    try:
-        warc_filename = warc['filename']
-        warc_url = warc['locations'][0]
-        warc_md5 = warc['checksums']['md5']
-        warc_collection = warc['collection']
-    except (KeyError, IndexError):
-        aip.log(log_path, f'WARC information is formatted wrong. JSON from API:\n {warc}\n '
-                          f'This WARC and its metadata reports will not be downloaded.')
-        continue
-
-    # Calculates the job id from the WARC filename.
-    try:
-        regex_job_id = re.match(r"^.*-JOB(\d+)", warc_filename)
-        job_id = regex_job_id.group(1)
-    except AttributeError:
-        aip.log(log_path, "Cannot calculate the WARC job id.")
-        continue
-
-    # Saves relevant information the WARC's seed in variables for future use.
-    # Stops processing if the WARC does not the required metadata.
-    try:
-        aip_id = seed_metadata[seed_id][0]
-        aip_title = seed_metadata[seed_id][1]
-    except (KeyError, IndexError):
-        aip.log(log_path, f"This WARC's seed is missing required metadata. JSON from API:\n {seed_metadata}\n"
-                          f"This WARC will not be downloaded.")
-        continue
-
-    # Adds the seed to the seed_to_aip dictionary. This is used for checking the downloaded AIPs for completeness.
-    seed_to_aip[seed_id] = aip_id
-
-    # Adds the AIP title to the aip_to_title dictionary. This is used for making the preservation.xml later.
-    aip_to_title[aip_id] = aip_title
-
-    # Makes the AIP directory for the seed's AIP (AIP folder with metadata and objects subfolders).
-    web.make_aip_directory(aip_id)
-
-    # Downloads the seed metadata from Archive-It into the seed's metadata folder.
-    web.download_metadata(aip_id, warc_collection, job_id, seed_id, date_end, log_path)
-
-    # Downloads the WARC from Archive-It into the seed's objects folder.
-    web.download_warc(aip_id, warc_filename, warc_url, warc_md5, date_end, log_path)
-
-# Checks for empty metadata or objects folders in the AIPs. These happens if there were uncaught download errors.
-web.find_empty_directory(log_path)
-
-
-# PART TWO: CREATE AIPS THAT ARE READY FOR INGEST INTO ARCHIVE
-
-# Makes directories used to store script outputs, if they aren't already there.
-aip.make_output_directories()
-
-# Starts counts for tracking script progress. Some processes are slow, so this shows the script is still working.
-current_aip = 0
-total_aips = len(os.listdir('.'))
-
-# Adds name for the next section to the log.
-aip.log(log_path, f'\n\nPROCESSING AIPS ({total_aips} TOTAL)')
-
-# Runs the scripts for each step of making an AIP, one folder at a time. Checks if the AIP is still present before
-# running each script, in case it was moved due to an error in the previous script.
-for aip_folder in os.listdir('.'):
-
-    # Updates the current AIP number and displays the script progress.
-    current_aip += 1
-    aip.log(log_path, f'\n{aip_folder}')
-    print(f'\nProcessing {aip_folder} ({current_aip} of {total_aips}).')
-
-    # Calculates the department group name in ARCHive from the folder name (the AIP ID).
-    # If it does not match the expected pattern, moves the AIP to an error folder and begins processing the next AIP.
-    if aip_folder.startswith('harg'):
-        department = 'hargrett'
-    elif aip_folder.startswith('magil'):
-        department = 'magil'
-    elif aip_folder.startswith('rbrl'):
-        department = 'russell'
-    else:
-        aip.log(log_path, f'AIP ID {aip_folder} does not start with an expected department prefix. '
-                          f'AIP moved to error folder.')
-        aip.move_error('department_prefix', aip_folder)
-        continue
-
-    # Extracts technical metadata from the files using FITS.
-    if aip_folder in os.listdir('.'):
-        aip.extract_metadata(aip_folder, f'{c.script_output}/{aips_directory}', log_path)
-
-    # Transforms the FITS metadata into the PREMIS preservation.xml file using saxon and xslt stylesheets.
-    if aip_folder in os.listdir('.'):
-        aip.make_preservationxml(aip_folder, aip_to_title[aip_folder], department, 'website', log_path)
-
-    # Bags the aip.
-    if aip_folder in os.listdir('.'):
-        aip.bag(aip_folder, log_path)
-
-    # Tars, and zips the aip.
-    if f'{aip_folder}_bag' in os.listdir('.'):
-        aip.package(aip_folder, os.getcwd(), zip=True)
-
-# Makes MD5 manifests of all AIPs the in this download using md5deep, with one manifest per department.
-aip.make_manifest()
-
-# Verifies the AIPs are complete and no extra AIPs were created. Does not look at the errors folder, so any AIPs with
-# errors will show as missing. Saves the result as a csv in the folder with the downloaded AIPs.
-print('\nStarting completeness check.')
-web.check_aips(date_end, date_start, seed_to_aip, log_path)
-print('\nFinished completeness check. See completeness_check_YYYY-MM-DD.csv for details.')
-
-# Adds completion of the script to the log.
-aip.log(log_path, f'\nScript finished running at {datetime.datetime.today()}.')
-
-# Moves script output folders (aips-to-ingest, errors, fits-xml, and preservation-xml) into the AIPs folder for this
-# download to keep everything together if another set is downloaded before these are deleted.
-os.chdir(c.script_output)
-to_move = ['aips-to-ingest', 'errors', 'fits-xml', 'preservation-xml',
-           f'web_preservation_download_log_{date_end}.txt']
-for item in os.listdir('.'):
-    if item in to_move:
-        os.replace(item, f'{aips_directory}/{item}')
+#     # Calculates seed id, which is a portion of the WARC filename between "-SEED" and "-".
+#     # Stops processing this WARC and starts the next if the WARC filename doesn't match expected pattern.
+#     try:
+#         regex_seed_id = re.match(r'^.*-SEED(\d+)-', warc['filename'])
+#         seed_id = regex_seed_id.group(1)
+#     except AttributeError:
+#         aip.log(log_path, f"Cannot calculate seed id from the WARC filename: {warc['filename']}.\n"
+#                           f"This WARC and its metadata reports will not be downloaded.")
+#         continue
+#
+#     # Saves relevant information about the WARC in variables for future use.
+#     # Stops processing if the WARC does not have the required metadata.
+#     try:
+#         warc_filename = warc['filename']
+#         warc_url = warc['locations'][0]
+#         warc_md5 = warc['checksums']['md5']
+#         warc_collection = warc['collection']
+#     except (KeyError, IndexError):
+#         aip.log(log_path, f'WARC information is formatted wrong. JSON from API:\n {warc}\n '
+#                           f'This WARC and its metadata reports will not be downloaded.')
+#         continue
+#
+#     # Calculates the job id from the WARC filename.
+#     try:
+#         regex_job_id = re.match(r"^.*-JOB(\d+)", warc_filename)
+#         job_id = regex_job_id.group(1)
+#     except AttributeError:
+#         aip.log(log_path, "Cannot calculate the WARC job id.")
+#         continue
+#
+#     # Saves relevant information the WARC's seed in variables for future use.
+#     # Stops processing if the WARC does not the required metadata.
+#     try:
+#         aip_id = seed_metadata[seed_id][0]
+#         aip_title = seed_metadata[seed_id][1]
+#     except (KeyError, IndexError):
+#         aip.log(log_path, f"This WARC's seed is missing required metadata. JSON from API:\n {seed_metadata}\n"
+#                           f"This WARC will not be downloaded.")
+#         continue
+#
+#     # Adds the seed to the seed_to_aip dictionary. This is used for checking the downloaded AIPs for completeness.
+#     seed_to_aip[seed_id] = aip_id
+#
+#     # Adds the AIP title to the aip_to_title dictionary. This is used for making the preservation.xml later.
+#     aip_to_title[aip_id] = aip_title
+#
+#     # Makes the AIP directory for the seed's AIP (AIP folder with metadata and objects subfolders).
+#     web.make_aip_directory(aip_id)
+#
+#     # Downloads the seed metadata from Archive-It into the seed's metadata folder.
+#     web.download_metadata(aip_id, warc_collection, job_id, seed_id, date_end, log_path)
+#
+#     # Downloads the WARC from Archive-It into the seed's objects folder.
+#     web.download_warc(aip_id, warc_filename, warc_url, warc_md5, date_end, log_path)
+#
+# # Checks for empty metadata or objects folders in the AIPs. These happens if there were uncaught download errors.
+# web.find_empty_directory(log_path)
+#
+#
+# # PART TWO: CREATE AIPS THAT ARE READY FOR INGEST INTO ARCHIVE
+#
+# # Makes directories used to store script outputs, if they aren't already there.
+# aip.make_output_directories()
+#
+# # Starts counts for tracking script progress. Some processes are slow, so this shows the script is still working.
+# current_aip = 0
+# total_aips = len(os.listdir('.'))
+#
+# # Adds name for the next section to the log.
+# aip.log(log_path, f'\n\nPROCESSING AIPS ({total_aips} TOTAL)')
+#
+# # Runs the scripts for each step of making an AIP, one folder at a time. Checks if the AIP is still present before
+# # running each script, in case it was moved due to an error in the previous script.
+# for aip_folder in os.listdir('.'):
+#
+#     # Updates the current AIP number and displays the script progress.
+#     current_aip += 1
+#     aip.log(log_path, f'\n{aip_folder}')
+#     print(f'\nProcessing {aip_folder} ({current_aip} of {total_aips}).')
+#
+#     # Calculates the department group name in ARCHive from the folder name (the AIP ID).
+#     # If it does not match the expected pattern, moves the AIP to an error folder and begins processing the next AIP.
+#     if aip_folder.startswith('harg'):
+#         department = 'hargrett'
+#     elif aip_folder.startswith('magil'):
+#         department = 'magil'
+#     elif aip_folder.startswith('rbrl'):
+#         department = 'russell'
+#     else:
+#         aip.log(log_path, f'AIP ID {aip_folder} does not start with an expected department prefix. '
+#                           f'AIP moved to error folder.')
+#         aip.move_error('department_prefix', aip_folder)
+#         continue
+#
+#     # Extracts technical metadata from the files using FITS.
+#     if aip_folder in os.listdir('.'):
+#         aip.extract_metadata(aip_folder, f'{c.script_output}/{aips_directory}', log_path)
+#
+#     # Transforms the FITS metadata into the PREMIS preservation.xml file using saxon and xslt stylesheets.
+#     if aip_folder in os.listdir('.'):
+#         aip.make_preservationxml(aip_folder, aip_to_title[aip_folder], department, 'website', log_path)
+#
+#     # Bags the aip.
+#     if aip_folder in os.listdir('.'):
+#         aip.bag(aip_folder, log_path)
+#
+#     # Tars, and zips the aip.
+#     if f'{aip_folder}_bag' in os.listdir('.'):
+#         aip.package(aip_folder, os.getcwd(), zip=True)
+#
+# # Makes MD5 manifests of all AIPs the in this download using md5deep, with one manifest per department.
+# aip.make_manifest()
+#
+# # Verifies the AIPs are complete and no extra AIPs were created. Does not look at the errors folder, so any AIPs with
+# # errors will show as missing. Saves the result as a csv in the folder with the downloaded AIPs.
+# print('\nStarting completeness check.')
+# web.check_aips(date_end, date_start, seed_to_aip, log_path)
+# print('\nFinished completeness check. See completeness_check_YYYY-MM-DD.csv for details.')
+#
+# # Adds completion of the script to the log.
+# aip.log(log_path, f'\nScript finished running at {datetime.datetime.today()}.')
+#
+# # Moves script output folders (aips-to-ingest, errors, fits-xml, and preservation-xml) into the AIPs folder for this
+# # download to keep everything together if another set is downloaded before these are deleted.
+# os.chdir(c.script_output)
+# to_move = ['aips-to-ingest', 'errors', 'fits-xml', 'preservation-xml',
+#            f'web_preservation_download_log_{date_end}.txt']
+# for item in os.listdir('.'):
+#     if item in to_move:
+#         os.replace(item, f'{aips_directory}/{item}')
