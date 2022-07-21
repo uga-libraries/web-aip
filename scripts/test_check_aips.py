b3c2a6e42b995ee2e840a569bc9e1a2f9da164c3
Adriane M Hanson <ahansonuga@gmail.com>
2022-07-21 10:32:20 -0400

    Add missing seed to test
    2472041 is in the API for the date bounds and needs to be included for an accurate test. Added to seed_df (and sorted row order by seed id to make it easier to find/add in the future) and adjusted the order that tests are made to be in seed order as much as possible.

--- scripts/test_check_aips.py
+++ scripts/test_check_aips.py
@@ -12,6 +12,7 @@ import shutil
 import configuration as c
 import web_functions as web
 
+
 def make_file(path):
     """Make a test file in the location and name indicated by the path.
     This is used a lot of times in the script and makes it easier to read what files are being created."""
@@ -29,19 +30,21 @@ aips_directory = os.path.join(c.script_output, f"aips_{date_end}")
 os.makedirs(aips_directory)
 os.chdir(aips_directory)
 
-# Dataframe with seed and AIP ids to use for making the test AIPs.
+# Makes a dataframe with seed and AIP ids to use for making the test AIPs.
 # In the full script, there is a lot more in the seed dataframe, but this function only needs the ids.
-seed_df = pd.DataFrame({"Seed_ID": ["2529671", "2529669", "2529633", "2529665", "2529634", "2529660", "2529642",
-                                    "2529627", "2529652", "2529631", "2529668", "2529681", "2529676", "2529629"],
-                        "AIP_ID": ["magil-ggp-2529671-2022-03", "magil-ggp-2529669-2022-03", "magil-ggp-2529633-2022-03",
-                                   "magil-ggp-2529665-2022-03", "magil-ggp-2529634-2022-03", "magil-ggp-2529660-2022-03",
-                                   "magil-ggp-2529642-2022-03","magil-ggp-2529627-2022-03", "magil-ggp-2529652-2022-03",
-                                   "magil-ggp-2529631-2022-03", "magil-ggp-2529668-2022-03", "magil-ggp-2529681-2022-03",
-                                   "magil-ggp-2529676-2022-03", "magil-ggp-2529629-2022-03"]})
+seed_df = pd.DataFrame({"Seed_ID": ["2472041", "2529627", "2529629", "2529631", "2529633", "2529634",
+                                    "2529642", "2529652", "2529660", "2529665", "2529668", "2529669",
+                                    "2529671", "2529676", "2529681"],
+                        "AIP_ID": ["magil-ggp-2472041-2022-03", "magil-ggp-2529627-2022-03", "magil-ggp-2529629-2022-03",
+                                   "magil-ggp-2529631-2022-03", "magil-ggp-2529633-2022-03", "magil-ggp-2529634-2022-03",
+                                   "magil-ggp-2529642-2022-03","magil-ggp-2529652-2022-03", "magil-ggp-2529660-2022-03",
+                                   "magil-ggp-2529665-2022-03", "magil-ggp-2529668-2022-03", "magil-ggp-2529669-2022-03",
+                                   "magil-ggp-2529671-2022-03", "magil-ggp-2529676-2022-03", "magil-ggp-2529681-2022-03"]
+                        })
 
 # Makes a minimum correct AIP for each of the seeds as a start for the test.
 # The folder is named _bag but is not a real bag and the files are plain text rather than actually downloaded.
-# It saves time and is enough to pass the tests, which are based on folder and file names.
+# This saves time and is enough to pass the tests, which are based on folder and file names.
 for aip in seed_df["AIP_ID"]:
     metadata_folder = os.path.join(f"{aip}_bag", "data", "metadata")
     os.makedirs(metadata_folder)
@@ -57,17 +60,17 @@ for aip in seed_df["AIP_ID"]:
     os.makedirs(objects_folder)
     make_file(f"{objects_folder}/test.warc")
 
-# Creates additional files for examples of any files that are allowed to repeat.
+# Creates additional files for examples of files that are allowed to repeat.
 # These AIPs, as well as the first AIP which is left as minimum correct, will pass the completeness check.
-make_file("magil-ggp-2529629-2022-03_bag/data/metadata/magil-ggp-2529629-2022-03_456123_crawldef.csv")
-make_file("magil-ggp-2529631-2022-03_bag/data/metadata/magil-ggp-2529631-2022-03_123789_crawljob.csv")
-make_file("magil-ggp-2529631-2022-03_bag/data/metadata/magil-ggp-2529631-2022-03_378912_crawljob.csv")
-make_file("magil-ggp-2529633-2022-03_bag/data/metadata/magil-ggp-2529633-2022-03_files-deleted_2022-04-05_del.csv")
+make_file("magil-ggp-2529627-2022-03_bag/data/metadata/magil-ggp-2529629-2022-03_456123_crawldef.csv")
+make_file("magil-ggp-2529629-2022-03_bag/data/metadata/magil-ggp-2529631-2022-03_123789_crawljob.csv")
+make_file("magil-ggp-2529629-2022-03_bag/data/metadata/magil-ggp-2529631-2022-03_378912_crawljob.csv")
+make_file("magil-ggp-2529631-2022-03_bag/data/metadata/magil-ggp-2529633-2022-03_files-deleted_2022-04-05_del.csv")
 make_file("magil-ggp-2529634-2022-03_bag/data/metadata/test2.warc_fits.xml")
 make_file("magil-ggp-2529634-2022-03_bag/data/objects/test2.warc")
 
 # Creates incorrect AIPs by adding or deleting files or folders.
-shutil.rmtree("magil-ggp-2529642-2022-03_bag")
+shutil.rmtree("magil-ggp-2529633-2022-03_bag")
 os.remove("magil-ggp-2529652-2022-03_bag/data/metadata/magil-ggp-2529652-2022-03_coll.csv")
 os.remove("magil-ggp-2529652-2022-03_bag/data/metadata/magil-ggp-2529652-2022-03_collscope.csv")
 os.remove("magil-ggp-2529660-2022-03_bag/data/metadata/magil-ggp-2529660-2022-03_seed.csv")
@@ -82,7 +85,7 @@ make_file("magil-ggp-2529681-2022-03_bag/data/objects/extra.txt")
 os.makedirs("magil-ggp-extra1-2022-03_bag")
 os.makedirs("magil-ggp-extra2-2022-03_bag")
 
-# # Verifies the AIPs are complete and no extra AIPs were created. Does not look at the errors folder, so any AIPs with
-# # errors will show as missing. Saves the result as a csv in the folder with the downloaded AIPs.
-# # todo: seed_to_aip needs to be seed_df
-# web.check_aips(date_end, date_start, seed_to_aip, aips_directory)
+# Runs the function being tested by this script.
+# Verifies the AIPs are complete and no extra AIPs were created.
+# Saves the result as a csv in the folder with the downloaded AIPs.
+web.check_aips(date_end, date_start, seed_df, aips_directory)
