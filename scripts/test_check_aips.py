6df9753e2ce38e75fa2b4d6a563a5a8d838893c5
Adriane M Hanson <ahansonuga@gmail.com>
2022-07-21 12:42:51 -0400

    Make a df with expected results

--- scripts/test_check_aips.py
+++ scripts/test_check_aips.py
@@ -5,7 +5,7 @@ and develop this function, which is not dependent on the data from the previous 
 Usage: python path/test_check_aips.py
 """
 
-import datetime
+import numpy as np
 import os
 import pandas as pd
 import shutil
@@ -37,10 +37,9 @@ seed_df = pd.DataFrame({"Seed_ID": ["2472041", "2529627", "2529629", "2529631", 
                                     "2529671", "2529676", "2529681"],
                         "AIP_ID": ["magil-ggp-2472041-2022-03", "magil-ggp-2529627-2022-03", "magil-ggp-2529629-2022-03",
                                    "magil-ggp-2529631-2022-03", "magil-ggp-2529633-2022-03", "magil-ggp-2529634-2022-03",
-                                   "magil-ggp-2529642-2022-03","magil-ggp-2529652-2022-03", "magil-ggp-2529660-2022-03",
+                                   "magil-ggp-2529642-2022-03", "magil-ggp-2529652-2022-03", "magil-ggp-2529660-2022-03",
                                    "magil-ggp-2529665-2022-03", "magil-ggp-2529668-2022-03", "magil-ggp-2529669-2022-03",
-                                   "magil-ggp-2529671-2022-03", "magil-ggp-2529676-2022-03", "magil-ggp-2529681-2022-03"]
-                        })
+                                   "magil-ggp-2529671-2022-03", "magil-ggp-2529676-2022-03", "magil-ggp-2529681-2022-03"]})
 
 # Makes a minimum correct AIP for each of the seeds as a start for the test.
 # The folder is named _bag but is not a real bag and the files are plain text rather than actually downloaded.
@@ -90,3 +89,33 @@ os.makedirs("magil-ggp-extra2-2022-03_bag")
 # Verifies the AIPs are complete and no extra AIPs were created.
 # Saves the result as a csv in the folder with the downloaded AIPs.
 web.check_aips(date_end, date_start, seed_df, aips_directory)
+
+# Makes a dataframe with the expected values for the check_aips() function output to use for comparison.
+aips = ["magil-ggp-2472041-2022-03", "magil-ggp-2529671-2022-03", "magil-ggp-2529669-2022-03", "magil-ggp-2529633-2022-03",
+        "magil-ggp-2529665-2022-03", "magil-ggp-2529634-2022-03", "magil-ggp-2529660-2022-03", "magil-ggp-2529629-2022-03",
+        "magil-ggp-2529642-2022-03", "magil-ggp-2529627-2022-03", "magil-ggp-2529652-2022-03", "magil-ggp-2529631-2022-03",
+        "magil-ggp-2529668-2022-03", "magil-ggp-2529681-2022-03", "magil-ggp-2529676-2022-03",
+        "magil-ggp-extra1-2022-03_bag", "magil-ggp-extra2-2022-03_bag"]
+urls = ["https://www.legis.ga.gov/", "https://grec.state.ga.us/", "https://oig.georgia.gov/", "https://medicalboard.georgia.gov/",
+        "https://ltgov.georgia.gov/", "https://consumer.georgia.gov/", "https://dso.georgia.gov/", "https://gbp.georgia.gov/",
+        "https://dcs.georgia.gov/", "https://gaa.georgia.gov/", "https://gdna.georgia.gov/", "https://gceo.georgia.gov/",
+        "https://oca.georgia.gov/", "https://gspc.georgia.gov/", "https://gsba.georgia.gov/", np.nan, np.nan]
+folders_made = ["False", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "Not expected", "Not expected"]
+colls = [np.nan, "True", "True", "True", "True", "True", "True", "True", "False", "True", "True", "True", "True", "True", "True", np.nan, np.nan]
+collscopes = [np.nan, "True", "True", "True", "True", "True", "True", "True", "True", "True", "False", "True", "True", "True", "True", np.nan, np.nan]
+seeds = [np.nan, "True", "True", "True", "True", "True", "False", "True", "True", "True", "True", "True", "True", "True", "True", np.nan, np.nan]
+seedscopes = [np.nan, "True", "True", "True", "True", "True", "False", "True", "True", "True", "True", "True", "True", "True", "True", np.nan, np.nan]
+crawldefs = [np.nan, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, np.nan, np.nan]
+crawljobs = [np.nan, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, np.nan, np.nan]
+preservation_xmls = [np.nan, "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "False", "True", "True", np.nan, np.nan]
+warcs = [np.nan, "False", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", np.nan, np.nan]
+object_extras = [np.nan, "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "False", "True", np.nan, np.nan]
+fits = [np.nan, "False", "False", "True", "True", "True", "True", "True", "True", "True", "True", "True", "False", "True", "True", np.nan, np.nan]
+metadata_extras = [np.nan, "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "True", "False", np.nan, np.nan]
+
+expected_df = pd.DataFrame({"AIP": aips, "URL": urls, "AIP Folder Made": folders_made, "coll.csv": colls,
+                            "collscope.csv": collscopes, "seed.csv": seeds, "seedscope.csv": seedscopes,
+                            "crawldef.csv count": crawldefs, "crawljob.csv count": crawljobs,
+                            "preservation.xml": preservation_xmls,	"WARC Count Correct": warcs,
+                            "Objects is all WARCs": object_extras, "fits.xml Count Correct": fits,
+                            "No Extra Metadata": metadata_extras})
