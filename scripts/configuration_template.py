0723da185df4d7849b2d0133109f74b07d0e889e
Adriane M Hanson <ahansonuga@gmail.com>
2022-07-27 16:04:17 -0400

    Reorganize file
    General explanation was in the middle of the document instead of the top.

--- scripts/configuration_template.py
+++ scripts/configuration_template.py
@@ -1,3 +1,14 @@
+"""
+To use, make a copy of this file named configuration.py and update the paths to match your local machine.
+    C: may need to be replaced with a different drive letter.
+    INSERT\\PATH\\ is replaced with the path on your local machine.
+    # is replaced by the number for your version of the programs.
+
+In a Mac or Linux environment:
+    Use the path to fits.sh instead of fits.bat
+    Change \\ to /
+"""
+
 # Folder for script output
 script_output = 'C:\\INSERT\\PATH\\'
 
@@ -10,18 +21,6 @@ password = 'INSERT-PASSWORD'
 
 # Local paths to programs used in the AIP functions.
 # The first three are dependencies and the stylesheets folder is part of the General AIP GitHub repo.
-
-"""
-To use, make a copy of this file named configuration.py and update the paths to match your local machine.
-    C: may need to be replaced with a different drive letter.
-    INSERT\\PATH\\ is replaced with the path on your local machine.
-    # is replaced by the number for your version of the programs.
-
-In a Mac or Linux environment:
-    Use the path to fits.sh instead of fits.bat
-    Change \\ to /
-"""
-
 FITS = 'C:\\INSERT\\PATH\\fits.bat'
 SAXON = 'C:\\INSERT\\PATH\\saxon-he-#.#.jar'
 MD5DEEP = 'C:\\INSERT\\PATH\\md5deep64.exe'
