84ca0678974515e591fb4fe1288f59fc8a39d16b
Adriane Hanson <ahansonuga@gmail.com>
2023-11-09 15:57:40 -0500

    Delete setUp
    There is only one test, so ok to have this all in the unit test.

--- tests/test_reset_seed.py
+++ tests/test_reset_seed.py
@@ -14,36 +14,6 @@ from web_functions import reset_seed
 
 class TestResetSeed(unittest.TestCase):
 
-    def setUp(self):
-        """
-        Makes everything needed for test input.
-        """
-        # Makes a seed folder with some files in the current directory.
-        # Only makes seed 2222222, since it is the one being reset for the test.
-        # These file are placeholders for what would usually be in the folder.
-        os.mkdir("2222222")
-        with open(os.path.join(os.getcwd(), "2222222", "metadata.csv"), "w") as file:
-            file.write("Metadata Placeholder")
-        with open(os.path.join(os.getcwd(), "2222222", "ARCHIVEIT.warc"), "w") as warc:
-            warc.write("WARC unzip correctly placeholder")
-        with open(os.path.join(os.getcwd(), "2222222", "ARCHIVEIT1.warc.open"), "w") as warc1:
-            warc1.write("WARC unzip error placeholder")
-
-        # Makes seed_df with one completed seed and one that was in progress (later logging fields have no data).
-        self.seed_df = pd.DataFrame([["aip-1", "1111111", "12345", "1000000", 0.521, 1, "ARCHIVEIT.warc.gz",
-                                      "Success", "No empty reports", "Success", "Success", "Success",
-                                      "Success", np.NaN],
-                                     ["aip-2", "2222222", "12345", "2000000", 0.522, 2,
-                                      "ARCHIVEIT.warc.gz|ARCHIVEIT-1.warc.gz",
-                                      "Success", "seed.csv", "Success", "Success", "Success", "Error", np.NaN]],
-                                    columns=["AIP_ID", "Seed_ID", "AIT_Collection", "Job_ID", "Size_GB", "WARCs",
-                                             "WARC_Filenames", "Metadata_Report_Errors", "Metadata_Report_Empty",
-                                             "Seed_Report_Redaction", "WARC_Download_Errors", "WARC_Fixity_Errors",
-                                             "WARC_Unzip_Errors", "Complete"])
-
-        # Makes a log, seeds_log.csv, in the script output directory.
-        self.seed_df.to_csv(os.path.join(config.script_output, "seeds_log.csv"), index=False)
-
     def tearDown(self):
         """
         Deletes the seed folder, if present, and the seeds_log.csv file.
@@ -58,24 +28,44 @@ class TestResetSeed(unittest.TestCase):
         Tests that the function correctly deletes the seed folder, updates seeds_df,
         and updates seeds_log.csv.
         """
-        reset_seed("2222222", self.seed_df)
+        # Makes everything needed for test input:
+        # a folder with placeholders for downloaded files, seed_df, and seeds_log.csv
+        os.mkdir("2222222")
+        with open(os.path.join(os.getcwd(), "2222222", "metadata.csv"), "w") as file:
+            file.write("Metadata Placeholder")
+        with open(os.path.join(os.getcwd(), "2222222", "ARCHIVEIT.warc"), "w") as warc:
+            warc.write("WARC unzip correctly placeholder")
+        with open(os.path.join(os.getcwd(), "2222222", "ARCHIVEIT1.warc.open"), "w") as warc1:
+            warc1.write("WARC unzip error placeholder")
+        seed_df = pd.DataFrame([["aip-1", "1111111", "12345", "1000000", 0.521, 1, "ARCHIVEIT.warc.gz", "Success",
+                                 "No empty reports", "Success", "Success", "Success", "Success", np.NaN],
+                                ["aip-2", "2222222", "12345", "2000000", 0.522, 2,
+                                 "ARCHIVEIT.warc.gz|ARCHIVEIT-1.warc.gz", "Success", "seed.csv", "Success", "Success",
+                                 "Success", "Error", np.NaN]],
+                               columns=["AIP_ID", "Seed_ID", "AIT_Collection", "Job_ID", "Size_GB", "WARCs",
+                                        "WARC_Filenames", "Metadata_Report_Errors", "Metadata_Report_Empty",
+                                        "Seed_Report_Redaction", "WARC_Download_Errors", "WARC_Fixity_Errors",
+                                        "WARC_Unzip_Errors", "Complete"])
+        seed_df.to_csv(os.path.join(config.script_output, "seeds_log.csv"), index=False)
+
+        # Runs the function being tested.
+        reset_seed("2222222", seed_df)
 
         # Test that the seed folder was deleted.
         seed_path = os.path.exists(os.path.join(os.getcwd(), "2222222"))
         self.assertEqual(seed_path, False, "Problem with test that the seed folder was deleted")
 
         # Test that the dataframe has the correct values.
-        self.seed_df = self.seed_df.fillna("BLANK")
-        actual_dataframe = [self.seed_df.columns.tolist()] + self.seed_df.values.tolist()
-        expected_dataframe = [["AIP_ID", "Seed_ID", "AIT_Collection", "Job_ID", "Size_GB", "WARCs", "WARC_Filenames",
-                               "Metadata_Report_Errors", "Metadata_Report_Empty", "Seed_Report_Redaction",
-                               "WARC_Download_Errors", "WARC_Fixity_Errors", "WARC_Unzip_Errors", "Complete"],
-                              ["aip-1", "1111111", "12345", "1000000", 0.521, 1, "ARCHIVEIT.warc.gz",
-                               "Success", "No empty reports", "Success", "Success", "Success", "Success", "BLANK"],
-                              ["aip-2", "2222222", "12345", "2000000", 0.522, 2,
-                               "ARCHIVEIT.warc.gz|ARCHIVEIT-1.warc.gz",
-                               "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK"]]
-        self.assertEqual(actual_dataframe, expected_dataframe, "Problem with test for dataframe values")
+        seed_df = seed_df.fillna("BLANK")
+        actual_df = [seed_df.columns.tolist()] + seed_df.values.tolist()
+        expected_df = [["AIP_ID", "Seed_ID", "AIT_Collection", "Job_ID", "Size_GB", "WARCs", "WARC_Filenames",
+                        "Metadata_Report_Errors", "Metadata_Report_Empty", "Seed_Report_Redaction",
+                        "WARC_Download_Errors", "WARC_Fixity_Errors", "WARC_Unzip_Errors", "Complete"],
+                       ["aip-1", "1111111", "12345", "1000000", 0.521, 1, "ARCHIVEIT.warc.gz",
+                        "Success", "No empty reports", "Success", "Success", "Success", "Success", "BLANK"],
+                       ["aip-2", "2222222", "12345", "2000000", 0.522, 2, "ARCHIVEIT.warc.gz|ARCHIVEIT-1.warc.gz",
+                        "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK"]]
+        self.assertEqual(actual_df, expected_df, "Problem with test for dataframe values")
 
         # Test that the CSV has the correct values.
         csv_path = os.path.join(config.script_output, "seeds_log.csv")
